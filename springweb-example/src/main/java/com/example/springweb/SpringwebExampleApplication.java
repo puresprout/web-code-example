057422db5a4887a5379dc24d526acb7f84cd8ad0
SungHyun Park <puresprout@gmail.com>
2018-09-19 23:13:35 +0900

    스프링 스타터 초기 커밋

---
+++ springweb-example/src/main/java/com/example/springweb/SpringwebExampleApplication.java
@@ -0,0 +1,12 @@
+package com.example.springweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwebExampleApplication.class, args);
+	}
+}
